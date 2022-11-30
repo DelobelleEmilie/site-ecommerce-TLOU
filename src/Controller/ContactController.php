7c35f9b3d930eecf1c046defafcbd179c5b26112
superpandachu666 <114162546+superpandachu666@users.noreply.github.com>
2022-11-30 13:59:28 +0100

    d

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,6 +8,7 @@ class ContactController extends AbstractController
 {
     #show() qui est publique = n'importe qui peut l'appeler
     # la fonction, elle affiche la page twig home.html.twig
+    #Si confirm vaut vrai, alors la page twig affiche le message de confirmation
     public function show() {
 
         if (!empty($_POST))
