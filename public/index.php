cc0f96ddf3190fdf5cfd2727fb2bfbe1addae07d
emilie_delobelle <114162546+DelobelleEmilie@users.noreply.github.com>
2023-02-06 10:57:33 +0100

    comment

--- public/index.php
+++ public/index.php
@@ -23,7 +23,8 @@ $router->post('/contact', 'contact#show');
 $router->get('/messageenvoie', 'messageenvoie#show');
 
 $router->get('/cart', 'cart#show');
-
+//créer la route.
+//Id doit être un entier, et quantity aussi
 $router->post('/cart/:id/:quantity', 'cart#add')->with('id', '[1-9][0-9]*')->with('quantity', '[1-9][0-9]*');
 
 #region user
