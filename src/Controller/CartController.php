eb8a9e7f9702fc0f8db3293df52bf40dd0b00552
Rémy Vissyrias <remy.vissyrias@gmail.com>
2023-02-07 22:14:22 +0100

    Fix PHPMailer

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,8 +4,12 @@
 namespace App\Controller;
 
 
+use App\Repository\ProductRepository;
+
 class CartController extends AbstractController
 {
+    private ProductRepository $productRepository;
+
     public function __construct($router)
     {
         parent::__construct($router);
@@ -14,33 +18,35 @@ class CartController extends AbstractController
             $_SESSION['cart'] = [];
         }
 
+        $this->productRepository = new ProductRepository();
     }
 
+    public function show()
+    {
+        $cart = $_SESSION['cart'];
+        $products = $this->productRepository->findAll();
 
-    function show() {
-        $cart = $_SESSION['cart']; // tableau 'id' => 'quantity'
-        $product = $this->productRepository->findAll();
+        $data = [];
 
-
-        $data =
-            foreach ($cart as $id => $quantity) {
-                foreach ($product as $product) {
-                    if ($product['id'] == $id) {
-                        $product['quantity'] = $quantity;
-                        $product['total'] = $quantity * ($product['price'] ?? 0);
-                        $data[] = $product;
-                        break;
-                    }
+        foreach ($cart as $id => $quantity) {
+            foreach ($products as $product) {
+                if ($product['id'] == $id) {
+                    $product['quantity'] = $quantity;
+                    $product['total'] = $quantity * ($product['price'] ?? 0);
+                    $data[] = $product;
+                    break;
                 }
             }
+        }
 
-  $this->render(
-      'cart/show.html.twig'
-      [
-      'data' => $data;
-    ]
-  )
-}
+        $this->render(
+            'cart/show.html.twig',
+            [
+                'products' => $data
+            ]
+        );
+    }
+
     public function add($id, $quantity = 1) {
         $cart = $_SESSION['cart'];
         if(isset($cart[$id])) {
