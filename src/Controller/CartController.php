818e80b91c84510e01ed1ed89cd594727a821eb6
emilie_delobelle <114162546+DelobelleEmilie@users.noreply.github.com>
2023-02-07 22:12:20 +0100

    panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -16,17 +16,31 @@ class CartController extends AbstractController
 
     }
 
-    public function show()
-    {
-        $this->render(
-            'cart/show.html.twig',
-            [
-//                juste pour transformer un objet en texte (format Jason)
-                'data' => json_encode($_SESSION['cart'])
-            ]
-        );
-    }
 
+    function show() {
+        $cart = $_SESSION['cart']; // tableau 'id' => 'quantity'
+        $product = $this->productRepository->findAll();
+
+
+        $data =
+            foreach ($cart as $id => $quantity) {
+                foreach ($product as $product) {
+                    if ($product['id'] == $id) {
+                        $product['quantity'] = $quantity;
+                        $product['total'] = $quantity * ($product['price'] ?? 0);
+                        $data[] = $product;
+                        break;
+                    }
+                }
+            }
+
+  $this->render(
+      'cart/show.html.twig'
+      [
+      'data' => $data;
+    ]
+  )
+}
     public function add($id, $quantity = 1) {
         $cart = $_SESSION['cart'];
         if(isset($cart[$id])) {
