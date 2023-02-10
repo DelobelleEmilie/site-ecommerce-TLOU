a6a76b550cc30ed52999205a89f6e380acef2757
emilie_delobelle <114162546+DelobelleEmilie@users.noreply.github.com>
2023-02-10 09:05:40 +0100

    mysql

--- exemple_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-//mes instructions
-Echo"Bienvenue sur le site!"
-?>
-<h1>Mon super titre</h1>
-<?php
-//autres instructions
-?>
--- exemple_2.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Bienvenue sur le site";
-echo "<h1> Mon super titre</h1>";
-?>
