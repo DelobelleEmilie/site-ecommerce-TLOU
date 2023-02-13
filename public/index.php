ca4717cb2f925f183af2d009c11f9dae4e41605e
emilie_delobelle <114162546+DelobelleEmilie@users.noreply.github.com>
2023-02-13 16:02:18 +0100

    routes

--- public/index.php
+++ public/index.php
@@ -13,100 +13,100 @@ session_start();
 $router = new Router(isset($_GET['url']) ? $_GET['url'] : '');
 
 #fonction router recuper l'url qui affiche la page twig
-$router->get('/', 'home#show');
+$router->get('/', 'home#show' ) ->roles(['ROLE_ADMIN', 'ROLE_USER']);;
 
 
 //page de contact
-$router->get('/contact', 'contact#show');
-$router->post('/contact', 'contact#show');
+$router->get('/contact', 'contact#show') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
+$router->post('/contact', 'contact#show')->roles(['ROLE_ADMIN', 'ROLE_USER']);;
 
-$router->get('/messageenvoie', 'messageenvoie#show');
+$router->get('/messageenvoie', 'messageenvoie#show')->roles(['ROLE_ADMIN', 'ROLE_USER']);;
 
 //panier
-$router->get('/cart', 'cart#show');
-$router->post('/cart', 'cart#show');
+$router->get('/cart', 'cart#show')->roles(['ROLE_ADMIN', 'ROLE_USER']);
+$router->post('/cart', 'cart#show')->roles(['ROLE_ADMIN', 'ROLE_USER']);
 
 //créer la route.
 //Id doit être un entier, et quantity aussi
-$router->post('/cart/:id/:quantity', 'cart#add')->with('id', '[1-9][0-9]*')->with('quantity', '[1-9][0-9]*');
+$router->post('/cart/:id/:quantity', 'cart#add')->with('id', '[1-9][0-9]*')->with('quantity', '[1-9][0-9]*') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
 
 #region user
-$router->get('/connexion','user#login');
-$router->post('/connexion','user#login');
+$router->get('/connexion','user#login') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
+$router->post('/connexion','user#login') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
 
 //page deconnexion
-$router->get('/logout','user#logout');
+$router->get('/logout','user#logout')->roles(['ROLE_ADMIN', 'ROLE_USER']) ;
 
 //page d'inscription
-$router->get('/inscription','user#register');
-$router->post('/inscription','user#register');
+$router->get('/inscription','user#register') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
+$router->post('/inscription','user#register') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
 
 //page de profil
-$router->get('/profile', 'user#profile');
-$router->post('/profile', 'user#profile');
+$router->get('/profile', 'user#profile') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
+$router->post('/profile', 'user#profile') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
 
 //page password lost
-$router->get('/lostpassword', 'user#lostpassword');
-$router->post('/lostpassword', 'user#lostpassword');
+$router->get('/lostpassword', 'user#lostpassword') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
+$router->post('/lostpassword', 'user#lostpassword') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
 
 # Affiche la liste des utilisateurs
-$router->get('/user', 'user#showList');
+$router->get('/user', 'user#showList') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
 
 # Affiche le category :id
-$router->get('/user/:id', 'user#show')->with('id', '[1-9][0-9]*');
+$router->get('/user/:id', 'user#show')->with('id', '[1-9][0-9]*') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
 
 # Affiche un formulaire vide
-$router->get('/user/create', 'user#edit', 'user#create');
+$router->get('/user/create', 'user#edit', 'user#create') ->roles(['ROLE_ADMIN', 'ROLE_USER']);
 
 # Enregistre le nouveau category
 $router->post('/user/create', 'user#edit');
-$router->get('/user/:id/edit', 'user#edit')->with('id', '[1-9][0-9]*');
-$router->post('/user/:id/edit', 'user#edit')->with('id', '[1-9][0-9]*');
-$router->get('/user/:id/delete', 'user#delete')->with('id', '[1-9][0-9]*');
+$router->get('/user/:id/edit', 'user#edit')->with('id', '[1-9][0-9]*') ->roles(['ROLE_ADMIN']);
+$router->post('/user/:id/edit', 'user#edit')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
+$router->get('/user/:id/delete', 'user#delete')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
 #endregion
 
 #region category
 # Affiche la liste des category
-$router->get('/category', 'category#showList');
+$router->get('/category', 'category#showList')->roles(['ROLE_ADMIN']);
 
 # Affiche le category :id
-$router->get('/category/:id', 'category#show')->with('id', '[1-9][0-9]*');
+$router->get('/category/:id', 'category#show')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
 
 # Affiche un formulaire vide
-$router->get('/category/create', 'category#edit', 'category#create');
+$router->get('/category/create', 'category#edit', 'category#create')->roles(['ROLE_ADMIN']);
 
 # Enregistre le nouveau category
 $router->post('/category/create', 'category#edit');
-$router->get('/category/:id/edit', 'category#edit')->with('id', '[1-9][0-9]*');
-$router->post('/category/:id/edit', 'category#edit')->with('id', '[1-9][0-9]*');
-$router->get('/category/:id/delete', 'category#delete')->with('id', '[1-9][0-9]*');
+$router->get('/category/:id/edit', 'category#edit')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
+$router->post('/category/:id/edit', 'category#edit')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
+$router->get('/category/:id/delete', 'category#delete')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
 #endregion
 
 #region product
 # Affiche la liste des produits
-$router->get('/product', 'product#showList');
+$router->get('/product', 'product#showList')->roles(['ROLE_ADMIN']);
 # Affiche le produit :id
-$router->get('/product/:id', 'product#show')->with('id', '[1-9][0-9]*');
+$router->get('/product/:id', 'product#show')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
 # Affiche un formulaire vide
-$router->get('/product/create', 'product#edit', 'product#create');
+$router->get('/product/create', 'product#edit', 'product#create')->roles(['ROLE_ADMIN']);
 # Enregistre le nouveau produit
-$router->post('/product/create', 'product#edit');
+$router->post('/product/create', 'product#edit')->roles(['ROLE_ADMIN']);
 # Affiche un formulaire avec les données du produit :id à modifier
-$router->get('/product/:id/edit', 'product#edit')->with('id', '[1-9][0-9]*');
+$router->get('/product/:id/edit', 'product#edit')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
 # Enregistre les modifications sur le produit
-$router->post('/product/:id/edit', 'product#edit')->with('id', '[1-9][0-9]*');
+$router->post('/product/:id/edit', 'product#edit')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
 # Supprime le produit :id
-$router->get('/product/:id/delete', 'product#delete')->with('id', '[1-9][0-9]*');
+$router->get('/product/:id/delete', 'product#delete')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);;
 #endregion
 
 #region type
 $router->get('/type', 'type#showList');
-$router->get('/type/:id', 'type#show')->with('id', '[1-9][0-9]*');
-$router->get('/type/create', 'type#edit', 'type#create');
-$router->post('/type/create', 'type#edit');
-$router->get('/type/:id/edit', 'type#edit')->with('id', '[1-9][0-9]*');
-$router->post('/type/:id/edit', 'type#edit')->with('id', '[1-9][0-9]*');
-$router->get('/type/:id/delete', 'type#delete')->with('id', '[1-9][0-9]*');
+$router->get('/type/:id', 'type#show')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
+$router->get('/type/create', 'type#edit', 'type#create')->roles(['ROLE_ADMIN']);
+$router->post('/type/create', 'type#edit')->roles(['ROLE_ADMIN']);
+$router->get('/type/:id/edit', 'type#edit')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
+$router->post('/type/:id/edit', 'type#edit')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
+$router->get('/type/:id/delete', 'type#delete')->with('id', '[1-9][0-9]*')->roles(['ROLE_ADMIN']);
 #endregion
 
     # fonction qui va comparer l'url demandée, et toutes les adresses enregistrées
---
+++ src/services/upload.php
@@ -0,0 +1,95 @@
+<?php
+
+use App\Controller\AbstractController;
+use App\Core\Router\Router;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use App\Core\Security\AuthenticationManager;
+class upload extends AbstractController
+{
+    function upload($file): string|null
+    {
+        $uploads = [
+            'extensions' => ['png', 'jpg'],
+            'path' => 'uploads/',
+            'state' => false
+        ];
+
+        $file_path = null;
+
+        $file_name = null;
+
+        if (isset($file)) {
+            if (!empty($file['name'])) {
+                $file_upload = explode(".", $file['name']);
+                // Vérification de l'extension
+                # permet de vérifier la présence d’une valeur dans un tableau.
+                if (in_array($file_upload[count($file_upload) - 1],
+                    $uploads['extensions'])) {
+                    // Nettoyage des accents
+                    $file_name = strtr($file_upload[0],
+                        'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+                    // Nettoyage des caractères spéciaux
+                    $file_name = preg_replace('/([^.a-z0-9]+)/i', '_', $file_name);
+                    # permet de couper la chaine de caractères à chaque fois qu’un point sera présent.
+                    $file_name = $file_name . '.' .
+                        $file_upload[count($file_upload) - 1];
+                    $file_path = $uploads['path'] . $file_name;
+                    #’instruction de la condition s’attarde à vérifier qu’un fichier ayant le même nom n’existe pas.
+                    #Si aucun fichier du même nom n’est trouvé, l’envoi de fichier est autorisé à continuer
+                    #Si toutes les conditionssont vérifiées,le déplacement du fichier vers ledossier «uploads» situé dans le dossier «public» est autorisé. Le déplacement s’effectue
+
+                    if (!file_exists($file_path)) {
+                        // Déplacement du fichier
+# l’autorisation est effectuée en passant l’état de la variable «uploads» à vrai
+                        move_uploaded_file($file['tmp_name'], $file_path);
+                        $uploads['state'] = true;
+                    } else {
+                        $msg = "Une image avec ce nom existe déjà !";
+                    }
+                } else {
+                    $msg = "L'extension du fichier n'est pas acceptée!";
+                }
+            } else {
+                $msg = "Veuillez choisir un fichier !";
+            }
+
+
+            if (isset($_POST['submit'])) {
+                $file = $_FILES['file'];
+
+                $fileName = $_FILES['file']['name'];
+                $fileTmpName = $_FILES['file']['tmp_name'];
+                $fileSize = $_FILES['file']['size'];
+                $fileError = $_FILES['file']['error'];
+                $fileType = $_FILES['file']['type'];
+
+                $fileExt = explode('.', $fileName);
+                $fileActualExt = strtolower(end($fileExt));
+
+                $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+
+                if (in_array($fileActualExt, $allowed)) {
+                    if ($fileError === 0) {
+                        if ($fileSize < 1000000) {
+                            $fileNameNew = uniqid('', true) . "." . $fileActualExt;
+                            $fileDestination = 'uploads/' . $fileNameNew;
+                            move_uploaded_file($fileTmpName, $fileDestination);
+                            header("Location: index.php?uploadsuccess");
+                        } else {
+                            echo "Your file is too big!";
+                        }
+                    } else {
+                        echo "There was an error uploading your file!";
+                    }
+                } else {
+                    echo "You cannot upload files of this type!";
+                }
+            }
+
+
+        }
+
+        return $file_path;
+    }
+}
